e33fcb3b0a6e03e6df89e7ffd77a96f986b48f53
ren1244 <n1244506804@hotmail.com>
2018-07-09 20:15:03 +0800

    fix square brackets problem in bbcode

--- HighlightToBBCode/main.js
+++ HighlightToBBCode/main.js
@@ -1,3 +1,23 @@
+//
+if (!HTMLCanvasElement.prototype.toBlob) {
+   Object.defineProperty(HTMLCanvasElement.prototype, 'toBlob', {
+     value: function (callback, type, quality) {
+       var canvas = this;
+       setTimeout(function() {
+         var binStr = atob( canvas.toDataURL(type, quality).split(',')[1] ),
+         len = binStr.length,
+         arr = new Uint8Array(len);
+
+         for (var i = 0; i < len; i++ ) {
+            arr[i] = binStr.charCodeAt(i);
+         }
+
+         callback( new Blob( [arr], {type: type || 'image/png'} ) );
+       });
+     }
+  });
+}
+
 var pre=document.getElementById("preview");
 var inp=document.getElementById("input");
 var bbc=document.getElementById("bbcode");
@@ -102,7 +122,7 @@ function getBBCode(ele,bgcolor)
 		switch(nd.nodeType)
 		{
 		case Node.TEXT_NODE:
-			str+=nd.nodeValue;
+			str+=(nd.nodeValue).replace(/\[/g,"&#91;").replace(/\]/g,"&#93;");
 			break;
 		case Node.ELEMENT_NODE:
 			c_style=window.getComputedStyle(nd,null);
