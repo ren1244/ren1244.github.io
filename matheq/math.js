771d5c464b7c58e32a69704dd1418fb11907ba64
ren1244 <n1244506804@hotmail.com>
2018-10-18 23:49:23 +0800

    use ckeditor

--- matheq/math.js
+++ matheq/math.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded",function (evt){
 	var onchangeInput=initOnChangeInput();
-	document.addEventListener("change",onchangeInput);
-	document.addEventListener("keyup",onchangeInput);
+	//document.addEventListener("change",onchangeInput);
+	//document.addEventListener("keyup",onchangeInput);
 	document.getElementById("save").addEventListener("click",function (){
 		html2canvas(document.querySelector("#output")).then(canvas => {
 			var a=document.getElementById("picLink");
@@ -18,28 +18,38 @@ document.addEventListener("DOMContentLoaded",function (evt){
 	document.getElementById("getUrl").addEventListener("click",function (){
 		var pos=location.href.search(/\?/g);
 		var url=pos>=0?location.href.substr(0,pos):location.href;
-		url+="?"+document.getElementById("input").value.toUTF8Array().deflate().encodeBase64().replace(/\+/g,"-").replace(/\//g,"_");
+		url+="?"+CKEDITOR.instances.input.getData().toUTF8Array().deflate().encodeBase64().replace(/\+/g,"-").replace(/\//g,"_");
 		var urlDisplay=document.getElementById("url");
 		urlDisplay.parentElement.setAttribute("style","display:inline-block");
 		urlDisplay.value=url;
 		urlDisplay.select();
 	});
-	var pos=location.href.search(/\?/g);
-	var data;
-	if(pos<0)
-		return;
-	data=location.href.substr(pos+1);
-	data=data.replace(/-/g,"+").replace(/_/g,"/").decodeBase64();
-	data=data.inflate().toUTF8String();
-	document.getElementById("input").value=data;
-	onchangeInput();
+	window.addEventListener('resize',resize);
+	CKEDITOR.replace('input',{
+		on:{
+			'instanceReady':()=>{
+				resize();
+				var pos=location.href.search(/\?/g);
+				var data;
+				if(pos<0)
+					return;
+				data=location.href.substr(pos+1);
+				data=data.replace(/-/g,"+").replace(/_/g,"/").decodeBase64();
+				data=data.inflate().toUTF8String();
+				CKEDITOR.instances.input.setData(data);
+				console.log(data);
+				onchangeInput();
+			},
+			'change':onchangeInput
+		},
+		customConfig:'../cke_config.js'
+	});
 });
 
 function initOnChangeInput()
 {
 	var out=document.getElementById("output");
-	var inp=document.getElementById("input");
-	var ts=0,delay=300,flag=false;
+	var ts=0,delay=400,flag=false;
 	return function(evt)
 	{
 		ts=Date.now();
@@ -55,7 +65,7 @@ function initOnChangeInput()
 		var t=Date.now();
 		if(t-ts>=delay)
 		{
-			out.innerHTML=inp.value.replace(/\\\[/g,'<div class="eq-ib">\\[').replace(/\\\]/g,'\\]</div>');
+			out.innerHTML=CKEDITOR.instances.input.getData();
 			MathJax.Hub.Typeset();
 		}
 		else
@@ -64,4 +74,11 @@ function initOnChangeInput()
 			setTimeout(proc0,t+delay-ts);
 		}
 	}
+}
+function resize(evt)
+{
+	var h0=window.innerHeight,
+	    h1=document.getElementById("tools").getBoundingClientRect().height;
+	CKEDITOR.instances.input.resize("100%",(h0-h1-2));
+	document.getElementById("ct-output").style.height=(h0-h1-2)+"px";
 }
